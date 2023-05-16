45fd6ff9efba2c88a1007edade4705e9ec762aa1
sardar-ali <alisardar465@gmail.com>
2023-05-16 14:06:33 +0500

    fixed some issues

--- frontend/src/constant/index.js
+++ frontend/src/constant/index.js
@@ -22,7 +22,48 @@ const data = [
         image:"https://tse1.mm.bing.net/th?id=OIP.Ah8LHrAHkdfD19U-r366bAHaHV&pid=Api&P=0",
         price:450,
         rating:4,
-    }
+    },
+    {
+        _id:4,
+        name:"Zinger Burger4",
+        category:"burger",
+        image:"https://tse1.mm.bing.net/th?id=OIP.Ah8LHrAHkdfD19U-r366bAHaHV&pid=Api&P=0",
+        price:450,
+        rating:4,
+    },
+    {
+        _id:5,
+        name:"Kabab stuffer pizza",
+        category:"pizza",
+        image:"https://tse1.mm.bing.net/th?id=OIP.Ah8LHrAHkdfD19U-r366bAHaHV&pid=Api&P=0",
+        price:1800,
+        rating:4,
+    },
+    {
+        _id:6,
+        name:"Checken cheese stuffer",
+        category:"pizza",
+        image:"https://tse1.mm.bing.net/th?id=OIP.Ah8LHrAHkdfD19U-r366bAHaHV&pid=Api&P=0",
+        price:1750,
+        rating:4,
+    },
+     {
+        _id:7,
+        name:"Checken cheese stuffer",
+        category:"pizza",
+        image:"https://tse1.mm.bing.net/th?id=OIP.Ah8LHrAHkdfD19U-r366bAHaHV&pid=Api&P=0",
+        price:1750,
+        rating:4,
+    },
+    {
+        _id:8,
+        name:"Kabab stuffer",
+        category:"pizza",
+        image:"https://tse1.mm.bing.net/th?id=OIP.Ah8LHrAHkdfD19U-r366bAHaHV&pid=Api&P=0",
+        price:1850,
+        rating:4,
+    },
+    
 ]
 
 export default data;
